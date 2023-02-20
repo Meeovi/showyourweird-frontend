94b895e834bffac89fcc56a7baaaf4fb87a5cb7e
Sebastian hilton <bastianh86@outlook.com>
2023-02-19 20:28:39 -0800

    Initial commit

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from 'C:/Users/sehilton/Documents/github/Internal-Portfolio/Amazon-Starter-Template/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
